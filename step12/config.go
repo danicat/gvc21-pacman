40d02b7d00b1b25809527507b0a30833e6fb89f1
Daniela Petruzalek <daniela.petruzalek@gmail.com>
2021-12-07 03:09:08 +0000

    add step 12 code

---
+++ step12/config.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
+// Config holds the emoji configuration
+type Config struct {
+	Player   string `json:"player"`
+	Ghost    string `json:"ghost"`
+	Wall     string `json:"wall"`
+	Dot      string `json:"dot"`
+	Pill     string `json:"pill"`
+	Death    string `json:"death"`
+	Space    string `json:"space"`
+	UseEmoji bool   `json:"use_emoji"`
+}
+
+var cfg Config
+
+func loadConfig(file string) error {
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	decoder := json.NewDecoder(f)
+	err = decoder.Decode(&cfg)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func init() {
+	err := loadConfig("config.json")
+	if err != nil {
+		log.Fatalf("failed to load configuration: %v", err)
+	}
+}
---
+++ step12/input.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+)
+
+type InputHandler interface {
+	Read() string
+}
+
+type KBHandler struct {
+	input chan string
+}
+
+func NewKBHandler() *KBHandler {
+	input := make(chan string)
+	go func(ch chan<- string) {
+		for {
+			input, err := readInput()
+			if err != nil {
+				log.Println("error reading input:", err)
+				ch <- "ESC"
+			}
+			ch <- input
+		}
+	}(input)
+
+	return &KBHandler{input: input}
+}
+
+func (k KBHandler) Read() string {
+	var key string
+	select {
+	case key = <-k.input:
+	default:
+	}
+	return key
+}
+
+type RandomHandler struct {
+}
+
+func NewRandomHandler() *RandomHandler {
+	return &RandomHandler{}
+}
+
+func (r RandomHandler) Read() string {
+	dir := rand.Intn(4)
+	move := map[int]string{
+		0: "UP",
+		1: "DOWN",
+		2: "RIGHT",
+		3: "LEFT",
+	}
+	return move[dir]
+}
